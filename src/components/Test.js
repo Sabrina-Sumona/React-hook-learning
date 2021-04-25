1c74eac8474ea21bfea8b9f9608e512289993fb5
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-25 19:03:13 +0600

    using-function-call-to-change-state

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,32 +1,40 @@
 import React, { useState } from 'react';
 // useState is used to use states in functional component
 const Test = props => {
-    //passing empty str
-    const testState = useState("");
-    // //passing empty arr
-    // const testState = useState([]);
-    // //passing empty obj
-    // const testState = useState({});
-    // //passing zero
-    // const testState = useState(0);
-    // //passing str
-    // const testState = useState("SNS");
+    // //passing empty str
+    // const testState = useState("");
+    // // //passing empty arr
+    // // const testState = useState([]);
+    // // //passing empty obj
+    // // const testState = useState({});
+    // // //passing zero
+    // // const testState = useState(0);
+    // // //passing str
+    // // const testState = useState("SNS");
 
-    // // array
-    // console.log(testState);
-    // // value 
-    // console.log(testState[0]);
-    // // function
-    // console.log(testState[1]);
-    if (testState[0] === "") {
-        // set state
-        //function call and value pass
-        testState[1]("SNS")
+    // // // array
+    // // console.log(testState);
+    // // // value 
+    // // console.log(testState[0]);
+    // // // function
+    // // console.log(testState[1]);
+    // if (testState[0] === "") {
+    //     // set state
+    //     //function call and value pass
+    //     testState[1]("SNS")
+    // }
 
+    const myName = useState("");
+
+    const changeState = () => {
+        myName[1]("SNS")
     }
+
     return (
         <div>
-            <h3>{testState[0]}</h3>
+            <button onClick={changeState}>Show</button>
+            {/* <h3>{testState[0]}</h3> */}
+            <h3>{myName[0]}</h3>
         </div>
     );
 }
