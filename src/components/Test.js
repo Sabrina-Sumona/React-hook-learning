663b0668dbc74f53add86d073b4368c31b13acdf
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-25 19:07:44 +0600

    using-array-destructuring

--- src/components/Test.js
+++ src/components/Test.js
@@ -24,17 +24,33 @@ const Test = props => {
     //     testState[1]("SNS")
     // }
 
-    const myName = useState("");
+    // // functiona call to change state
+    // const myName = useState("");
+
+    // const changeState = () => {
+    //     myName[1]("SNS")
+    // }
+
+    // array-destructuring
+    const [myName, setMyName] = useState("");
+    const [myInfo, setMyInfo] = useState({});
 
     const changeState = () => {
-        myName[1]("SNS")
+        setMyName("Sumona");
+        setMyInfo({
+            age: 23,
+            address: "Dhaka, Bangladesh"
+        })
     }
 
     return (
         <div>
             <button onClick={changeState}>Show</button>
+            <h3>Name: {myName}</h3>
+            <h4>Age: {myInfo.age}</h4>
+            <h4>Address: {myInfo.address}</h4>
             {/* <h3>{testState[0]}</h3> */}
-            <h3>{myName[0]}</h3>
+            {/* <h3>{myName[0]}</h3> */}
         </div>
     );
 }
